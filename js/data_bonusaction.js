e01f7b1d588070e31a3b7f6fe378681dd3b4b0bb
Elke Heymans <el.heymans@gmail.com>
2022-09-06 22:09:41 +0200

    Added some extra items

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -1,36 +1,48 @@
 data_bonusaction = [
-    {
-        title: "Offhand Attack",
-        icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
-        description: "Attack with your off hand",
-        reference: "PHB, pgs. 192,194-195.",
-        bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
-        ]
-    },
-    {
-        title: "Cast a spell",
-        icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
-        description: "Cast a spell with a casting time of 1 bonus action",
-        reference: "PHB, pg. 192.",
-        bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "For further details, see the <i>Cast a spell</i> action."
-        ]
-    },
-    {
-        title: "Use class feature",
-        icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
-        description: "Use a racial or class feature that uses a bonus action",
-        reference: "See class page for more information.",
-        bullets: [
-
-        ]
-    }
-]
+  {
+    title: "Offhand Attack",
+    icon: "crossed-swords",
+    subtitle: "Use with the Attack action",
+    description: "Attack with your off hand",
+    reference: "PHB, pgs. 192,194-195.",
+    bullets: [
+      "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
+      "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
+      "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
+      "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it.",
+    ],
+  },
+  {
+    title: "Cast a spell",
+    icon: "magic-swirl",
+    subtitle: "Cast time of 1 bonus action",
+    description: "Cast a spell with a casting time of 1 bonus action",
+    reference: "PHB, pg. 192.",
+    bullets: [
+      "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
+      "For further details, see the <i>Cast a spell</i> action.",
+    ],
+  },
+  {
+    title: "Use class feature",
+    icon: "embrassed-energy",
+    subtitle: "Some features use bonus actions",
+    description: "Use a racial or class feature that uses a bonus action",
+    reference: "See class page for more information.",
+    bullets: [],
+  },
+  {
+    title: "Drink a potion",
+    icon: "potion-ball",
+    subtitle: "Roll for the effect",
+    description: "Roll the dice as per the description of the potion",
+    bullets: [],
+  },
+  {
+    title: "Command",
+    icon: "conversation",
+    subtitle: "Shout a word or short sentence",
+    description: "Text should fit within the time of 1 bonusaction.",
+    bullets: [],
+  },
+];
